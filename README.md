b16eedaaab054901fb102ab586ce812dcce34aae
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2022-10-07 07:00:35 -0500

    add example for custom endpoint

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a kitchen sink project to explore all the features of PocketBase.
 * [x] Saving Structure from comand line
 * [x] Restoring Structure from command line
 * [ ] Running automated tests
-* [ ] Custom Endpoint
+* [x] Custom Endpoint
 * [x] Custom Command
 * [ ] Custom SQL support
 * [ ] Websocket support
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/spf13/cobra"
 	"github.com/vagmi/hellopb/exporter"
@@ -12,6 +15,44 @@ import (
 
 func main() {
 	app := pocketbase.New()
+	addCommands(app)
+	setupHooks(app)
+	addEndpoints(app)
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addEndpoints(app *pocketbase.PocketBase) {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/hello",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello world!")
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				apis.RequireAdminOrUserAuth(),
+			},
+		})
+
+		return nil
+	})
+}
+
+func setupHooks(app *pocketbase.PocketBase) {
+	app.OnRecordAfterCreateRequest().Add(func(e *core.RecordCreateEvent) error {
+		if e.Record.Collection().Name == "invitations" {
+			err := invitations.SendInvitation(app, e.Record)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func addCommands(app *pocketbase.PocketBase) {
 	var filename string
 	exportCmd := &cobra.Command{
 		Use:       "export",
@@ -40,16 +81,4 @@ func main() {
 	importCmd.Flags().StringVarP(&filename, "from", "f", "collections.json", "Import from file")
 	app.RootCmd.AddCommand(exportCmd)
 	app.RootCmd.AddCommand(importCmd)
-	app.OnRecordAfterCreateRequest().Add(func(e *core.RecordCreateEvent) error {
-		if e.Record.Collection().Name == "invitations" {
-			err := invitations.SendInvitation(app, e.Record)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
 }
